f434f8d10394529830556409a43a2dbbc929ade1
ngmtine <ngmfries@gmail.com>
2023-11-19 16:05:02 +0900

    fix: pagesのurlみすってた

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: process.env.GITHUB_PAGES ? "table-virtuoso-example" : "./",
+    base: process.env.GITHUB_PAGES ? "table_virtuoso_example" : "./",
 });
