c6ca738057f1caebea47ba41b1670f39043addeb
ngmtine <ngmfries@gmail.com>
2023-11-19 18:07:31 +0900

    update: 戻るボタン押下イベント上書き

--- src/root/Root.tsx
+++ src/root/Root.tsx
@@ -1,16 +1,19 @@
-import { useState, CSSProperties } from "react";
+import { useState, useEffect, CSSProperties } from "react";
 import { ReactPlane } from "../react_plane/ReactPlane";
 import { ReactVirtuoso } from "../react_virtuoso/ReactVirtuoso";
 
+// ページタイプ
 type PageType = "Menu" | "ReactPlane" | "ReactVirtuoso";
 
-const Menu = ({ setPage }: { setPage: (page: PageType) => void }) => {
-    const clickableStyle: CSSProperties = {
-        textAlign: "left",
-        cursor: "pointer",
-        textDecoration: "underline",
-    };
+// liのスタイル定義
+const clickableStyle: CSSProperties = {
+    textAlign: "left",
+    cursor: "pointer",
+    textDecoration: "underline",
+};
 
+// メニューコンポーネント
+const Menu = ({ setPage }: { setPage: (page: PageType) => void }) => {
     return (
         <>
             <div>るーとぺーじ</div>
@@ -34,6 +37,19 @@ const Menu = ({ setPage }: { setPage: (page: PageType) => void }) => {
 
 export const Root = () => {
     const [page, setPage] = useState<PageType>("Menu");
+
+    // 戻るボタン押下イベント上書き
+    useEffect(() => {
+        window.history.pushState({ page }, "", page);
+
+        const handlePopState = (event: PopStateEvent) => {
+            if (event.state?.page) setPage(event.state.page);
+        };
+
+        window.addEventListener("popstate", handlePopState);
+
+        return () => window.removeEventListener("popstate", handlePopState);
+    }, [page]);
 
     if (page === "Menu") {
         return <Menu setPage={setPage} />;
